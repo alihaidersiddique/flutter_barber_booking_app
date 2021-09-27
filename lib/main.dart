a72bf67c33416cd175effa48052f6a356445e180
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2021-09-27 08:35:08 +0500

    init firebase and phone auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_ui/firebase_auth_ui.dart';
+import 'package:firebase_auth_ui/providers.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barber_booking_app/state/state_management.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Firebase.initializeApp();
+
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -27,40 +38,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey<ScaffoldState> scaffoldState = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+          key: scaffoldState,
           body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('assets/images/my_bg.png'),
-          fit: BoxFit.cover,
-        )),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.phone,
-                  color: Colors.white,
-                ),
-                label: Text(
-                  'LOGIN WITH PHONE',
-                  style: TextStyle(color: Colors.white),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.black),
-                ),
-              ),
-            )
-          ],
-        ),
-      )),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/my_bg.png'),
+              fit: BoxFit.cover,
+            )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  width: MediaQuery.of(context).size.width,
+                  child: ElevatedButton.icon(
+                    onPressed: () => processLogin(context),
+                    icon: Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      'LOGIN WITH PHONE',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
     );
+  }
+
+  processLogin(BuildContext context) {
+    var user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      FirebaseAuthUi.instance()
+          .launchAuth([AuthProvider.phone()]).then((firebaseUser) {
+        //refresh state
+        context.read(userLogged).state = FirebaseAuth.instance.currentUser;
+        ScaffoldMessenger.of(scaffoldState.currentContext!).showSnackBar(SnackBar(
+            content: Text(
+                'Login Success ${FirebaseAuth.instance.currentUser!.phoneNumber}')));
+        print('user login ok');
+      }).catchError((e) {
+        if (e is PlatformException) if (e.code ==
+            FirebaseAuthUi.kUserCancelledError)
+          ScaffoldMessenger.of(scaffoldState.currentContext!)
+              .showSnackBar(SnackBar(content: Text('${e.message}')));
+        else
+          ScaffoldMessenger.of(scaffoldState.currentContext!)
+              .showSnackBar(SnackBar(content: Text('Unk error')));
+      });
+    } else {}
   }
 }
